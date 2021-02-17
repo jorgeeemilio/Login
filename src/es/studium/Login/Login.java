c2332ac1ce331c3131f8f4890ea62428cb33e4da
Jorge Rodríguez <jorge@grupostudium.com>
2021-02-17 18:22:49 +0100

    Programación Modular+Clase BaseDatos

--- src/es/studium/Login/Login.java
+++ src/es/studium/Login/Login.java
@@ -10,6 +10,10 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Login implements WindowListener, ActionListener
 {
@@ -23,6 +27,12 @@ public class Login implements WindowListener, ActionListener
 	TextField txtClave = new TextField(20);
 	Button btnAceptar = new Button("Acceder");
 	Button btnLimpiar = new Button("Limpiar");
+	
+	BaseDatos bd;
+	String sentencia = "";
+	Connection connection = null;
+	Statement statement = null;
+	ResultSet rs = null;
 	
 	public Login()
 	{
@@ -64,10 +74,42 @@ public class Login implements WindowListener, ActionListener
 		else if(botonPulsado.getSource().equals(btnAceptar))
 		{
 			// Conectar BD
+			bd = new BaseDatos();
+			connection = bd.conectar();
 			// Buscar lo que el usuario ha escrito en los TextField
-			// Si existe en la BD, mostrar Menú Principal
-			// Si no existe en la BD, mostrar Diálogo de Error
+			sentencia = "SELECT * FROM usuarios WHERE nombreUsuario='"
+			+txtUsuario.getText()+"' AND claveUsuario = SHA2('"
+					+txtClave.getText()+"',256);";
+			try
+			{
+				//Crear una sentencia
+				statement = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,
+						ResultSet.CONCUR_READ_ONLY);
+				rs = statement.executeQuery(sentencia);
+				if(rs.next()) // Si ha encontrado algo
+				{
+					// Si existe en la BD, mostrar Menú Principal
+					int tipo = rs.getInt("tipoUsuario");
+					new MiSoftware(tipo);
+				}
+				else // Si no encuentra nada
+				{
+					// Si no existe en la BD, mostrar Diálogo de Error
+					dialogoLogin.setLayout(new FlowLayout());
+					dialogoLogin.add(lblError);
+					dialogoLogin.addWindowListener(this);
+					dialogoLogin.setSize(180,75);
+					dialogoLogin.setLocationRelativeTo(null);
+					dialogoLogin.setResizable(false);
+					dialogoLogin.setVisible(true);
+				}
+			}
+			catch (SQLException sqle)
+			{
+			}
+			
 			// Desconectar la BD
+			bd.desconectar(connection);
 		}
 	}
 
@@ -82,8 +124,14 @@ public class Login implements WindowListener, ActionListener
 	@Override
 	public void windowClosing(WindowEvent arg0)
 	{
-		// TODO Auto-generated method stub
-		System.exit(0);
+		if(dialogoLogin.isActive())
+		{
+			dialogoLogin.setVisible(false);
+		}
+		else
+		{
+			System.exit(0);
+		}
 	}
 
 	@Override
